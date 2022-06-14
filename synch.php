c3aec22fcbb6ed5f1bb78b44f55e59e99aa1ec4a
vvizzard <r.haritiana.z@gmail.com>
2022-06-14 14:38:51 +0300

    synch reponse et reponse_non_valide

--- synch.php
+++ synch.php
@@ -9,6 +9,20 @@ if ($table == "reponse") {
 	$date = $_GET["date"];
 	$reponse = get("SELECT * FROM reponse WHERE STR_TO_DATE(date, '%d-%m-%Y') > STR_TO_DATE('" . $date . "', '%d-%m-%Y')");
 }
+else if ($table == "reponse_non_valide") {
+	$userId = $_GET["user_id"];
+	$reponse = getParam(
+		"SELECT * FROM reponse_non_valide WHERE user_id in (
+			SELECT id FROM user WHERE district_id in (
+				SELECT id FROM district WHERE region_id in (
+					SELECT region_id FROM district WHERE id in (
+						SELECT district_id FROM user WHERE id = :userId
+					)
+				)
+			)
+		)", array("userId"=>$userId)
+	);
+}
 
 header('Content-Type: application/json; charset=utf-8');
 
